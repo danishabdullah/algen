9c62db3cf23bb72572fd98b19eebdeb94b80a484
Danish Abdullah <dev@danishabdullah.com>
2016-05-19 10:41:55 +0100

    added manifest.in and all postgres data types

--- algen/consts.py
+++ algen/consts.py
@@ -4,6 +4,9 @@ __author__ = "danishabdullah"
 
 __all__ = ("POSTGRES_TYPES", "MUTABLE_DICT_TYPES", "NO_PARAMS_TYPES")
 
-POSTGRES_TYPES = ('HSTORE', 'ARRAY', 'JSON', 'JSONB')
+POSTGRES_TYPES = ('ARRAY', 'BIGINT', 'BIT', 'BOOLEAN', 'BYTEA', 'CHAR', 'CIDR', 'DATE', 'DOUBLE_PRECISION', 'ENUM',
+                  'FLOAT', 'HSTORE', 'INET', 'INTEGER', 'INTERVAL', 'JSON', 'JSONB', 'MACADDR', 'NUMERIC', 'OID',
+                  'REAL', 'SMALLINT', 'TEXT', 'TIME', 'TIMESTAMP', 'UUID', 'VARCHAR', 'INT4RANGE', 'INT8RANGE',
+                  'NUMRANGE', 'DATERANGE', 'TSRANGE', 'TSTZRANGE', 'TS')
 MUTABLE_DICT_TYPES = ('HSTORE', 'JSON', 'JSONB')
 NO_PARAMS_TYPES = ('Integer', 'BigInteger', 'Float', 'Decimal', 'Numeric', 'JSONB', 'JSON', 'HSTORE')
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("LICENSE", 'r') as file:
 
 setup(
     name='algen',
-    version='0.9',
+    version='0.9.1',
     packages=find_packages(),
     url='https://github.com/danishabdullah/algen',
     install_requires=requirements,
